8fa3547561ae4f342a5410ea2b1b6b287b53ff39
18107 <18107@users.noreply.github.com>
2024-07-06 21:44:35 +0930

    Add files via upload

---
+++ QuitOverreacting/BreathControllerPatch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using VFX.PlayerHealth;
+
+namespace QuitOverreacting
+{
+    [HarmonyPatch(typeof(BreathController), "DetermineBreathType")]
+    internal class BreathControllerPatch
+    {
+        static void Prefix(CameraPostRenderSettings ___settings, ref float ____currentColdStrength, ref float ____currentHeatStrength, out float __state)
+        {
+            __state = ___settings.StartStrainAtOxygenLevel;
+            ___settings.StartStrainAtOxygenLevel = 0.001f;
+            ____currentColdStrength -= 0.99f;
+            ____currentHeatStrength -= 0.99f;
+        }
+
+        static void Postfix(CameraPostRenderSettings ___settings, ref float ____currentColdStrength, ref float ____currentHeatStrength, float __state)
+        {
+            ___settings.StartStrainAtOxygenLevel = __state;
+            ____currentColdStrength += 0.99f;
+            ____currentHeatStrength += 0.99f;
+        }
+    }
+}
---
+++ QuitOverreacting/VoidManagerPlugin.cs
@@ -0,0 +1,13 @@
+﻿using VoidManager.MPModChecks;
+
+namespace QuitOverreacting
+{
+    public class VoidManagerPlugin : VoidManager.VoidPlugin
+    {
+        public override MultiplayerType MPType => MultiplayerType.Client;
+
+        public override string Author => "18107";
+
+        public override string Description => "Stops player breathing noises when the player isn't taking damage";
+    }
+}
