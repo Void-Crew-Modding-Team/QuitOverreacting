4681cfffb9fc52f428e2b3bd2f72804641a88880
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-10-03 22:22:12 -0700

    Patched heat death temp for breathing.

---
+++ QuitOverreacting/HeatDeathPatch.cs
@@ -0,0 +1,14 @@
+﻿using CG.Game.Player;
+using HarmonyLib;
+
+namespace QuitOverreacting
+{
+    [HarmonyPatch(typeof(Player), "Awake")]
+    internal class HeatDeathPatch
+    {
+        static void Postfix(Player __instance)
+        {
+            __instance.Parameters.DeathToHeat = 120f;
+        }
+    }
+}
